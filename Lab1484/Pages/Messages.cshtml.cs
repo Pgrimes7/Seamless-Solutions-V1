ce5745eb3458aff4f698edd1590aca573a5c2dc0
abbystavridis <abby.stavridis@gmail.com>
2025-02-28 22:32:40 -0500

    Added "Inbox" button that goes to Messages page

---
+++ Lab1484/Pages/Messages.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1484.Pages
+{
+    public class MessagesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
