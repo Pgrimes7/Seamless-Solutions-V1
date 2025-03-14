6ac8d92f1645db9cd0be075b8c00fced442ea747
Pgrimes7 <pgrimes7@outlook.com>
2025-03-13 23:46:42 -0400

    account singup and fixing validations

---
+++ Lab1484/Pages/AccountSignUp.cshtml.cs
@@ -0,0 +1,64 @@
+using System.Runtime.InteropServices;
+using Lab1484.Pages.DataClasses;
+using Lab1484.Pages.DB;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1484.Pages
+{
+    public class AccountSignUpModel : PageModel
+    {
+        [BindProperty]
+        public User NewUser { get; set; } = new User();
+
+        public IActionResult OnGet()
+        {
+            //Check to see if the user is logged in
+            string currentUser = HttpContext.Session.GetString("username");
+            //Redirect them if they aren't
+            if (string.IsNullOrEmpty(currentUser))
+            {
+                return RedirectToPage("/Login");
+            }
+
+            return Page();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            DBClass.InsertUser(NewUser);
+            DBClass.Lab2DBConnection.Close();
+
+            return RedirectToPage("/CreateUser");
+        }
+
+        //Method with sample data for the populate button
+        public IActionResult OnPostPopulateHandler()
+        {
+            ModelState.Clear();
+            NewUser.UserType = 3;
+            NewUser.firstName = "Rob";
+            NewUser.lastName = "Johnson";
+            NewUser.email = "robjohnson@example.com";
+            NewUser.phone = "123-246-2864";
+            NewUser.username = "johnsron";
+            NewUser.password = "67478";
+
+            return Page();
+        }
+        public IActionResult OnPostClear()
+        {
+            // Clears the form 
+            ModelState.Clear();
+            NewUser = new User();
+            return RedirectToPage();
+        }
+    }
+}
--- Lab1484/Pages/DataClasses/Grant.cs
+++ Lab1484/Pages/DataClasses/Grant.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1484.Pages.DataClasses
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1484.Pages.DataClasses
 {
     public class Grant
     {
@@ -11,7 +13,9 @@
         public String businessName { get; set; }
 
         public String? category { get; set; }
-        public DateTime submissionDate { get; set; }
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "Date must be between 01/01/1753 and 12/31/9999")]
+        public DateTime? submissionDate { get; set; }
         public DateTime? awardDate { get; set; }
         public String? grantStatus { get; set; }
         public double amount { get; set; }
