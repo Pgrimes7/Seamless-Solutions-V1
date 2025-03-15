ea6638980c01cdf6641fcf4ddf4852d4ab056e08
MichaelDonati <michaeldonati.1223@gmail.com>
2025-03-14 21:39:05 -0400

    Fixed ProjectCreation

--- Lab1484/Pages/DataClasses/Project.cs
+++ Lab1484/Pages/DataClasses/Project.cs
@@ -1,17 +1,30 @@
-﻿namespace Lab1484.Pages.DataClasses
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1484.Pages.DataClasses
 {
     public class Project
     {
         public int ProjectID { get; set; }
-        public String? ProjectName { get; set; }
+        public String ProjectName { get; set; }
         public double ProjectCost { get; set; }
-        public int? ProjectAdminID { get; set; }
+        public int ProjectAdminID { get; set; }
 
         public String? AssignedEmployee { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateCompleted { get; set; }
 
-        public DateTime DateDue { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "Award Date must be between 01/01/1753 and 12/31/9999")]
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "Award Date must be between 01/01/1753 and 12/31/9999")]
+        public DateTime? DateCompleted { get; set; } = DateTime.Now;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1/1/1753", "12/31/9999", ErrorMessage = "Award Date must be between 01/01/1753 and 12/31/9999")]
+        public DateTime? DateDue { get; set; }
 
         public String AdminName { get; set; }
 
