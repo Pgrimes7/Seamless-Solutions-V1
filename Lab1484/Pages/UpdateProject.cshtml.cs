acc091fcfa6040964c80c64680e182dad7bfcd8f
MichaelDonati <michaeldonati.1223@gmail.com>
2025-03-30 17:31:41 -0400

    Added UpdateProject (working on functionality)

---
+++ Lab1484/Pages/UpdateProject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1484.Pages
+{
+    public class UpdateProjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
