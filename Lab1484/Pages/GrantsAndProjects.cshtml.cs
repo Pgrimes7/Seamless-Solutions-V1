6c74cefc877322257aaef9c6ae3b35ac558700dd
abbystavridis <abby.stavridis@gmail.com>
2025-03-30 21:45:20 -0400

    added grants and projects page to sidebar nav

---
+++ Lab1484/Pages/GrantsAndProjects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1484.Pages
+{
+    public class GrantsAndProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
