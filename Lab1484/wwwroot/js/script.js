c5b93ebcc6a3e5bc26e1a072baab9d40d9c0dc2f
abbystavridis <abby.stavridis@gmail.com>
2025-04-11 15:38:04 -0400

    donut chart on dashboard linked to grant status counts

--- Lab1484/wwwroot/js/script.js
+++ Lab1484/wwwroot/js/script.js
@@ -1,41 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //donut chart
-    var grantsCtx = document.getElementById('grantsChart').getContext('2d');
-    var grantsChart = new Chart(grantsCtx, {
-        type: 'doughnut',
-        data: {
-            labels: ['Active', 'Potential', 'Funded', 'Archived', 'Rejected'],
-            datasets: [{
-                label: 'Grant Status',
-                data: [10, 5, 15, 30, 3],
-                backgroundColor: [
-                    '#450084', //active, purple
-                    '#F4EFE1', //potential, light gold
-                    '#CBB677', //funded, gold
-                    '#DACCE6',  //archived, light purple
-                    '#BF0603'  //rejected, red
-                ],
-                hoverOffset: 4
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            cutout: '70%',
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Grant Status',
-                    font: {
-                        size: 17
-                    },
-                  
-                }
-            }
-        }
-    });
+    fetch('/Dashboard?handler=GrantStatusData')
+        .then(response => response.json())
+        .then(data => {
+            var grantsCtx = document.getElementById('grantsChart').getContext('2d');
+            var grantsChart = new Chart(grantsCtx, {
+                type: 'doughnut',
+                data: {
+                    labels: ['Active', 'Potential', 'Funded', 'Archived', 'Rejected'],
+                    datasets: [{
+                        label: 'Grant Status',
+                        data: data,
+                        backgroundColor: [
+                            '#450084', //active, purple
+                            '#F4EFE1', //potential, light gold
+                            '#CBB677', //funded, gold
+                            '#DACCE6',  //archived, light purple
+                            '#BF0603'  //rejected, red
+                        ],
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    cutout: '70%',
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Grant Status',
+                            font: {
+                                size: 17
+                            },
 
-});
-               
-   
+                        }
+                    }
+                }
+            });
+
+        });
+})
