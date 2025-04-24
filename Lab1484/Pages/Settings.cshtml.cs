e6422a668f8bf1d96126d2da71fd4b90e8cc0f69
abbystavridis <abby.stavridis@gmail.com>
2025-04-24 15:04:13 -0400

    settings ting

--- Lab1484/Pages/Settings.cshtml.cs
+++ Lab1484/Pages/Settings.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Lab1484.Pages.Shared
+namespace Lab1484.Pages
 {
     public class SettingsModel : PageModel
     {
